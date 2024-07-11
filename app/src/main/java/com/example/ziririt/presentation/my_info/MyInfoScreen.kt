32c7ed3c576bbb2883c53faffe7f8b355509e8e6
sooj36 <sooj2023@gmail.com>
2024-07-11 23:06:09 +0900

    ZAOS-8 마이페이지 BOX 배치 하였습니다

--- app/src/main/java/com/example/ziririt/presentation/my_info/MyInfoScreen.kt
+++ app/src/main/java/com/example/ziririt/presentation/my_info/MyInfoScreen.kt
@@ -1,26 +1,73 @@
 package com.example.ziririt.presentation.my_info
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ziririt.ui.theme.ZiriritTheme
 
 @Composable
 fun MyInfoScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF141517)) // 동일한 배경 색상 설정
-    ) {
-        // BoardScreen의 내용
-    }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            // BoardScreen의 내용
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, start = 24.dp, bottom = 8.dp, end = 24.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally// Column 내 항목들 수직으로 가운데 정렬
+            ) {
+                Image(
+                    painterResource(id = com.example.ziririt.R.drawable.profile_setting),
+                    contentDescription = null
+                )
+                Image(
+                    painterResource(id = com.example.ziririt.R.drawable.profile_image),
+                    contentDescription = null
+                )
+
+                Text(text = "유저닉네임임", modifier = Modifier.padding(top = 8.dp), color = Color.White)
+                //
+            }
+        }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Divider(color = Color.White, thickness = 0.5.dp)
+
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+            contentAlignment = Alignment.CenterStart) {
+            Text(text = "나의 찌리릿", color = Color.White)
+        }
+    } //colummmmm
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun MyInfoScreenPreview() {
     ZiriritTheme {
